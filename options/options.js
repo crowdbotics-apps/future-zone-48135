6bd3da81c26601c3f0dbfff11779a768be905d04
Crowdbotics <team@crowdbotics.com>
2024-05-28 03:13:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "future-zone-48135",
+  url: "https://future-zone-48135.botics.co",
+  api: "https://future-zone-48135.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
